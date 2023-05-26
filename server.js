903be41467d9df711ea6762d4b9f6814e145f48c
Mariusz Politowicz <politowiczmar@gmail.com>
2023-05-26 22:53:18 +0200

    Export db file using app request

--- server.js
+++ server.js
@@ -10,23 +10,34 @@ mongoClient.connect(
   "mongodb://localhost:27017/",
   { useNewUrlParser: true, useUnifiedTopology: true },
   (err, client) => {
-    console.log("Successfully connected to the database");
-    const app = express();
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Successfully connected to the database");
 
-    app.use(cors());
-    app.use(express.json());
-    app.use(express.urlencoded({ extended: false }));
+      const db = client.db("companyDB");
+      const app = express();
 
-    app.use("/api", employeesRoutes);
-    app.use("/api", departmentsRoutes);
-    app.use("/api", productsRoutes);
+      app.use(cors());
+      app.use(express.json());
+      app.use(express.urlencoded({ extended: false }));
 
-    app.use((req, res) => {
-      res.status(404).send({ message: "Not found..." });
-    });
+      app.use((req, res, next) => {
+        req.db = db;
+        next();
+      });
 
-    app.listen("8000", () => {
-      console.log("Server is running on port: 8000");
-    });
+      app.use("/api", employeesRoutes);
+      app.use("/api", departmentsRoutes);
+      app.use("/api", productsRoutes);
+
+      app.use((req, res) => {
+        res.status(404).send({ message: "Not found..." });
+      });
+
+      app.listen("8000", () => {
+        console.log("Server is running on port: 8000");
+      });
+    }
   }
 );
